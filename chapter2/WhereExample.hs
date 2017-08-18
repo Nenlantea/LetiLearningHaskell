31e7675d03825c0348a6849bb9cb4e4573aa104d
Nenlantea <nenlantea@gmail.com>
2017-08-17 21:12:39 -0500

    Add an example of changing let for where :)

---
+++ chapter2/WhereExample.hs
@@ -0,0 +1,18 @@
+module Example where
+
+mult1      = x * y
+   where x = 5
+         y = 6
+
+adding     = a + 3 + b
+   where a = 3
+         b = 1000
+
+increasing = f * 5
+   where f = 10 * 5 + g
+         g = 10
+
+complex    = j / k + l
+   where k = 7
+         l = negate k
+         j = l * 10
