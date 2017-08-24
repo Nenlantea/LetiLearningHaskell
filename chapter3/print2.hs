3e092d9ecac71f44a2d3297e88781281cb1c5ae1
Nenlantea <nenlantea@gmail.com>
2017-08-23 22:36:17 -0500

    adding exercises for chapter 3 part 3.4

---
+++ chapter3/print2.hs
@@ -0,0 +1,8 @@
+module Print2 where
+
+main :: IO ()
+main = do
+  putStr   "Count to four for me."
+  putStr   "Sure. One, two"
+  putStr   ", three, and"
+  putStrLn " four!"
---
+++ chapter3/toplevelexercises.hs
@@ -0,0 +1,26 @@
+module TopLevelExercise where
+
+-- Exercise 1
+
+one       = x * y
+
+  where x = 5
+
+        y = 7
+
+
+one1 = a * b
+
+a = 5
+
+b = 7
+
+
+truth = one == one1
+
+-- Exercise 2 h not in scope
+
+-- Exercise 3 / 4
+
+area     d = pi * (r * r)
+   where r = d/2
