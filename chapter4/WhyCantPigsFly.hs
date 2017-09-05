add5d173a856103b557ff9e007d95c2af4f4574c
Nenlantea <nenlantea@gmail.com>
2017-09-05 14:25:34 -0500

    add the advance up until the explanation about IF

--- chapter4/WhyCantPigsFly.hs
+++ chapter4/WhyCantPigsFly.hs
@@ -12,3 +12,14 @@ changeMood :: Mood -> Mood
 --                Blah -> Woot
 changeMood Woot = Blah
 changeMood    _ = Woot
+
+a = not True && True
+
+b = not (x == 6) where
+    x = 2
+
+c = 1 * 2 > 5
+
+d = "Merry" > "Happy"
+
+e = ['1', '2', '3', ' '] ++ "Look at me!"
