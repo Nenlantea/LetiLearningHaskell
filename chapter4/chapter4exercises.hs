49bbb25e51c5c61203d78fcf9f6b4dcd21799c76
Nenlantea <nenlantea@gmail.com>
2017-09-11 00:36:08 -0500

    4.9 chapter exercises

---
+++ chapter4/chapter4exercises.hs
@@ -0,0 +1,42 @@
+module Chapter4 where
+
+awesome = ["Papuchon", "curry", ":)"]
+also = ["Quake", "The Simons"]
+allAwesome = [awesome, also]
+
+length' :: [a] -> Int
+length' a = length a
+
+a = length [1, 2, 3, 4, 5] == 5
+
+b = length [(1, 2), (2, 3), (3, 4)] == 3
+
+c = length allAwesome == 2
+
+d = length (concat allAwesome) == 5
+
+division = div 6 (length [1, 2, 3])
+
+testing a = if a + 3 == 5
+            then putStrLn "yey"
+            else putStrLn "poop"
+x = 5
+
+e = length allAwesome + length awesome == 5
+
+f = (8 == 8) && ('b' < 'a') == False
+
+isPalindrome :: (Eq a) => [a] -> IO ()
+
+isPalindrome z = if reverse z == z
+                 then putStrLn "This is a palindrome"
+                 else putStrLn "This is not a palindrome"
+
+myAbs :: Integer -> Integer
+
+myAbs a = if a > 0
+          then a
+          else (-a)
+
+g :: (a, b) -> (c, d) -> ((b, d), (a, c))
+g a b = ((snd a, snd b), (fst a, fst b)) 
---
+++ chapter4/tuples.hs
@@ -0,0 +1,7 @@
+module Tuple where
+
+tupFunc :: (Int, [a])
+        -> (Int, [a])
+        -> (Int, [a])
+tupFunc (a, b) (c, d) =
+        ((a + c),(b ++ d))
